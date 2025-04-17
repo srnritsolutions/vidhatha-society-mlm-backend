cf239a4cbc6586879df5ab523232addccb92ce0b
karunya <karunyalingutla02@gmail.com>
2025-04-17 16:30:54 +0530

    adding some changes in updateAfterpaymentSucces and LoginByEmailAndPassword() method

--- src/main/java/com/srnr/vidhatasocietymlm/user/dao/UserDAO.java
+++ src/main/java/com/srnr/vidhatasocietymlm/user/dao/UserDAO.java
@@ -8,4 +8,5 @@ public interface UserDAO
 {
 	Optional<User> saveUser(User user,String referralCode);
 	Optional<User> updateUserAfterPaymentSuccess(String userId, boolean paymentSuccess);
+	public Optional<User> loginByEmailAndPassword(String userEmail, String userPassword);
 }
