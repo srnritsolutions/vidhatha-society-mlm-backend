909cdf4da6cb861d4b20c8fde5e57cd003519c61
Vethendra <vethendra@example.com>
2025-04-23 09:36:12 +0530

    added changePasswordRequestDTO

---
+++ src/main/java/com/srnr/vidhatasocietymlm/user/dto/ChangePasswordRequestDTO.java
@@ -0,0 +1,34 @@
+package com.srnr.vidhatasocietymlm.user.dto;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ChangePasswordRequestDTO implements Serializable 
+{
+	@NotBlank(message = "Email can't be blank or null !")
+	@Pattern(regexp = "^[a-zA-Z][A-Za-z0-9._%+-]+@gmail\\.com$",message = "Invalid email")
+	private String  email;
+
+	@NotBlank(message = "Password can't be blank or null !")
+	@Size(min=6,message = "Password must be at least 6 characters")
+	private String newPassword;
+
+	@NotBlank(message = "Password can't be blank or null !")
+	@Size(min=6,message = "Password must be at least 6 characters")
+	private String confirmPassword;
+
+}
