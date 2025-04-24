0f3b13937767c0d6448b6fcca6316241dc8090f1
Vethendra <vethendra@example.com>
2025-04-24 10:37:24 +0530

    added edit image method in controller layer

--- src/main/java/com/srnr/vidhatasocietymlm/user/controller/UserController.java
+++ src/main/java/com/srnr/vidhatasocietymlm/user/controller/UserController.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.srnr.vidhatasocietymlm.repository.UserRepository;
 import com.srnr.vidhatasocietymlm.user.dto.ChangePasswordRequestDTO;
 import com.srnr.vidhatasocietymlm.user.dto.EmailRequestDTO;
 import com.srnr.vidhatasocietymlm.user.dto.LoginRequestDTO;
@@ -21,6 +23,7 @@ import com.srnr.vidhatasocietymlm.user.service.UserService;
 import com.srnr.vidhatasocietymlm.util.Message;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/user")
@@ -28,9 +31,6 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-
-	@Autowired
-	private UserRepository userRepository;
 
 	@PostMapping(value = "/create", consumes = { MediaType.APPLICATION_JSON_VALUE }, 
 			produces = {MediaType.APPLICATION_JSON_VALUE })
@@ -40,24 +40,7 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(registerUser);
 	}
 
-	@PostMapping(value = "/verifyEmail", consumes = { MediaType.APPLICATION_JSON_VALUE }, 
-			produces = {MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<?> verifyEmail(@Valid @RequestBody EmailRequestDTO emailRequestDTO)
-	{
-		Message verifyUserByEmail = this.userService.verifyUserByEmail(emailRequestDTO);
-		return new ResponseEntity<Message>(verifyUserByEmail,HttpStatus.OK);
-	}
-
-	@PostMapping(value = "/VerifyOTP", consumes = { MediaType.APPLICATION_JSON_VALUE }, 
-			produces = {MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<?> verifyOTP(@Valid @RequestBody VerifyOTPRequestDTO verifyOTPRequestDTO) 
-	{
-		 Message verifyOTP = this.userService.verifyOTP(verifyOTPRequestDTO);
-		return new ResponseEntity<Message>(verifyOTP,HttpStatus.OK);
-	}
-
-
-	@PostMapping(value = "/UpdateUserAfterPayment")
+	@PostMapping(value = "/updateUserAfterPayment")
 	public ResponseEntity<?> updateUserAfterPayment(@RequestParam String userId,@RequestParam boolean paymentSuccess )
 	{
 		String updateUserAfterPaymentSuccess = this.userService.updateUserAfterPaymentSuccess(userId, paymentSuccess);
@@ -74,8 +57,23 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.OK).body(new Message(loginUserByEmailAndPassword));
 	}
 
+	@PostMapping(value = "/verifyEmail", consumes = { MediaType.APPLICATION_JSON_VALUE }, 
+			produces = {MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<?> verifyEmail(@Valid @RequestBody EmailRequestDTO emailRequestDTO)
+	{
+		Message verifyUserByEmail = this.userService.verifyUserByEmail(emailRequestDTO);
+		return new ResponseEntity<Message>(verifyUserByEmail,HttpStatus.OK);
+	}
 
-	@PostMapping(value="/UpdatePassword", 
+	@PostMapping(value = "/verifyOTP", consumes = { MediaType.APPLICATION_JSON_VALUE }, 
+			produces = {MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<?> verifyOTP(@Valid @RequestBody VerifyOTPRequestDTO verifyOTPRequestDTO) 
+	{
+		Message verifyOTP = this.userService.verifyOTP(verifyOTPRequestDTO);
+		return new ResponseEntity<Message>(verifyOTP,HttpStatus.OK);
+	}
+
+	@PostMapping(value="/updatePassword", 
 			consumes = { MediaType.APPLICATION_JSON_VALUE},
 			produces = { MediaType.APPLICATION_JSON_VALUE }
 			)
@@ -83,6 +81,17 @@ public class UserController {
 	{
 		String updatePassword = userService.updatePassword(changePasswordRequestDTO);
 		return ResponseEntity.status(HttpStatus.OK).body(new Message(updatePassword));
+	}
+
+	@PutMapping(value = "/editProfileImage",
+			consumes = {MediaType.APPLICATION_JSON_VALUE},
+			produces = {MediaType.APPLICATION_JSON_VALUE}
+			)
+	public ResponseEntity<?> editProfileImage(MultipartFile file,
+			@PathVariable("userid") @NotBlank(message = "User ID can't be blank or null!") String userId) 
+	{
+		Message editUserImage = this.userService.editUserImage(file, userId);
+		return  ResponseEntity.status(HttpStatus.OK).body(editUserImage);
 	}
 
 }
